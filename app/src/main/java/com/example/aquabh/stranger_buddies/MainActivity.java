f3431f9952bffdde6de3a336c31da58951e5dc59
Aquabh <sharma.abhi03@yahoo.in>
2017-03-20 21:13:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/aquabh/stranger_buddies/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.aquabh.stranger_buddies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
